b7e673a6ba7dc036afbb08091536b2e222aed926
Thibaut Durand <durand.tibo@gmail.com>
2025-04-14 17:09:53 -0700

    Add `BaseIngestor`

---
+++ src/analora/ingestor/__init__.py
@@ -0,0 +1,7 @@
+r"""Contain data ingestors."""
+
+from __future__ import annotations
+
+__all__ = ["BaseIngestor", "is_ingestor_config", "setup_ingestor"]
+
+from analora.ingestor.base import BaseIngestor, is_ingestor_config, setup_ingestor
