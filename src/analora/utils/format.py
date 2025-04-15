5f7e863e17068367306c7902478385fa490c4cb7
Thibaut Durand <durand.tibo@gmail.com>
2025-04-14 23:32:52 -0700

    Add `human_byte` (#13)

--- src/analora/utils/format.py
+++ src/analora/utils/format.py
@@ -11,6 +11,38 @@ if TYPE_CHECKING:
     from collections.abc import Mapping
 
 
+def human_byte(size: float, decimal: int = 2) -> str:
+    r"""Return a human-readable string representation of byte sizes.
+
+    Args:
+        size: The number of bytes.
+        decimal: The number of decimal digits.
+
+    Returns:
+        The human-readable string representation of byte sizes.
+
+    Example usage:
+
+    ```pycon
+
+    >>> from analora.utils.format import human_byte
+    >>> human_byte(2)
+    '2.00 B'
+    >>> human_byte(2048)
+    '2.00 KB'
+    >>> human_byte(2097152)
+    '2.00 MB'
+
+    ```
+    """
+    for unit in ["B", "KB", "MB", "GB", "TB", "PB"]:
+        if abs(size) < 1024.0:
+            break
+        if unit != "PB":
+            size /= 1024.0
+    return f"{size:,.{decimal}f} {unit}"
+
+
 def human_time(seconds: float) -> str:
     r"""Return a number of seconds in an easier format to read
     ``hh:mm:ss``.
