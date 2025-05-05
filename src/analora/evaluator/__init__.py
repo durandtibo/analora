d888b5a1007b63e49bf3d3807c637c97b6fbe917
Thibaut Durand <durand.tibo@gmail.com>
2025-05-04 19:10:21 -0700

    Add `AccuracyEvaluator`

--- src/analora/evaluator/__init__.py
+++ src/analora/evaluator/__init__.py
@@ -2,9 +2,9 @@ r"""Contain evaluators."""
 
 from __future__ import annotations
 
-__all__ = ["BaseEvaluator", "BaseStateEvaluator", "Evaluator"]
+__all__ = ["AccuracyEvaluator", "BaseEvaluator", "BaseStateEvaluator", "Evaluator"]
 
-
+from analora.evaluator.accuracy import AccuracyEvaluator
 from analora.evaluator.base import BaseEvaluator
 from analora.evaluator.state import BaseStateEvaluator
 from analora.evaluator.vanilla import Evaluator
---
+++ src/analora/evaluator/accuracy.py
@@ -0,0 +1,50 @@
+r"""Implement the accuracy evaluator."""
+
+from __future__ import annotations
+
+__all__ = ["AccuracyEvaluator"]
+
+
+from analora.evaluator.state import BaseStateEvaluator
+from analora.metric import accuracy
+from analora.state.accuracy import AccuracyState
+
+
+class AccuracyEvaluator(BaseStateEvaluator[AccuracyState]):
+    r"""Implement the accuracy evaluator.
+
+    Args:
+        state: The state containing the ground truth and predicted
+            labels.
+
+    Example usage:
+
+    ```pycon
+
+    >>> import numpy as np
+    >>> from analora.evaluator import AccuracyEvaluator
+    >>> from analora.state import AccuracyState
+    >>> evaluator = AccuracyEvaluator(
+    ...     AccuracyState(
+    ...         y_true=np.array([1, 0, 0, 1, 1]),
+    ...         y_pred=np.array([1, 0, 0, 1, 1]),
+    ...         y_true_name="target",
+    ...         y_pred_name="pred",
+    ...     )
+    ... )
+    >>> evaluator
+    AccuracyEvaluator(
+      (state): AccuracyState(y_true=(5,), y_pred=(5,), y_true_name='target', y_pred_name='pred', nan_policy='propagate')
+    )
+    >>> evaluator.evaluate()
+    {'accuracy': 1.0, 'count_correct': 5, 'count_incorrect': 0, 'count': 5, 'error': 0.0}
+
+    ```
+    """
+
+    def _evaluate(self) -> dict[str, float]:
+        return accuracy(
+            y_true=self._state.y_true,
+            y_pred=self._state.y_pred,
+            nan_policy=self._state.nan_policy,
+        )
