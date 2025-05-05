55607d12bc577800a35a5c91bc0c7c14b6bab604
Thibaut Durand <durand.tibo@gmail.com>
2025-05-04 19:08:33 -0700

    Add `BaseStateEvaluator` (#85)

--- src/analora/evaluator/__init__.py
+++ src/analora/evaluator/__init__.py
@@ -2,10 +2,9 @@ r"""Contain evaluators."""
 
 from __future__ import annotations
 
-__all__ = [
-    "BaseEvaluator",
-    "Evaluator",
-]
+__all__ = ["BaseEvaluator", "BaseStateEvaluator", "Evaluator"]
+
 
 from analora.evaluator.base import BaseEvaluator
+from analora.evaluator.state import BaseStateEvaluator
 from analora.evaluator.vanilla import Evaluator
