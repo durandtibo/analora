a7ba7b2f901ec9ed8f621b12b5f546edc6a16e58
Thibaut Durand <durand.tibo@gmail.com>
2025-05-06 18:59:22 -0700

    Add `BaseOutput` (#96)

--- src/analora/output/__init__.py
+++ src/analora/output/__init__.py
@@ -1,1 +1,9 @@
 r"""Contain outputs."""
+
+from __future__ import annotations
+
+__all__ = ["BaseLazyOutput", "BaseOutput", "Output"]
+
+from analora.output.base import BaseOutput
+from analora.output.lazy import BaseLazyOutput
+from analora.output.vanilla import Output
